6693049b8078d9c051b44de7051c757995364eb8
beto <robertoneivacorvino@gmail.com>
2025-02-25 21:57:37 -0300

    Add files via upload

---
+++ src/ProjetoJoguinho/JOjinho.java
@@ -0,0 +1,23 @@
+package ProjetoJoguinho;
+
+public class JOjinho {
+    public static void main(String[] args) throws InterruptedException {
+        {
+            String corrida;
+
+
+            String space = "- ";
+            Thread.sleep(1000);
+
+            System.out.print(space);
+            for (int c = 0; c <= 10; c++) {
+
+                corrida = " " + space;
+                System.out.print(corrida);
+                space = " " + space;
+                System.out.println("Teste");
+
+            }
+        }
+    }
+}
